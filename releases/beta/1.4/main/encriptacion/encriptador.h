9f7a8be5a73c6ec678ab4474cebc8b24ef9bb003
Diego Rodríguez Riera <diegorodriguezriera@gmail.com>
2017-05-03 12:15:55 +0200

    Add files via upload

---
+++ releases/beta/1.4/main/encriptacion/encriptador.h
@@ -0,0 +1,12 @@
+#ifndef _ENCRIPTACION_
+#define _ENCRIPTACION_
+
+
+int encriptar(char *data_file,
+				char *user_key,
+				int zn,
+				int lvl_seguridad,
+				char* end_path);
+void multiplicar(int **clave, int vector[], int size, int zn);
+void imprimir_vector(int vector[], int size);
+#endif
